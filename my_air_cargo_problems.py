e8ef9fb2a69a1752acede6e99935c3a7dca194de
Shu Uesugi <shu@chibicode.com>
2017-07-20 15:02:51 +0900

    h_ignore_preconditions

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -204,8 +204,14 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+        # Load, Unload, and Fly all satisfy one literal at a time, so
+        # the heuristic = # of unsatisfied goals.
         count = 0
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
+        for clause in self.goal:
+            if clause not in kb.clauses:
+                count += 1
         return count
 
 
